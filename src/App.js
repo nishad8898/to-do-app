772b9fbe16b8f41436decb16d902bc5e584128c3
Sachin Nishad <nishadsachin8898@gmail.com>
2022-07-01 01:11:25 +0530

    created to do app on reactjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/logo.svg";
+import blob from "./assets/blob.svg";
+import "./App.scss";
+import ListCollection from "./component/ListCollection/ListCollection";
+import TitleBox from "./component/TitleBox/TitleBox";
+import InputBox from "./component/InputBox/InputBox";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-container">
+        <div className="circle"></div>
+        <div className="blob">
+          <img src={blob} alt="blob" />
+        </div>
+        <div className="main-box">
+          <TitleBox image={logo} title="to-do-list" />
+          <InputBox />
+          <ListCollection title="to-do's" />
+        </div>
+      </div>
     </div>
   );
 }
